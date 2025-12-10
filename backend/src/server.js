f4b9ab99f52604c4d2c3349e07045158b076d76e
Joel Scaria <joelscaria241444@gmail.com>
2025-12-10 17:45:03 +0530

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { ENV } from './lib/env.js';
+
+const app = express ();
+
+app.listen(ENV.PORT, () => {
+    console.log(`Server running on port ${ENV.PORT}`);
+})
