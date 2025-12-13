bd28abc0f3a77869c6c7e2071c6e3569cf5568b3
Joel Scaria <joelscaria241444@gmail.com>
2025-12-13 18:42:07 +0530

    db and tools setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import { ENV } from './env.js';
+
+export const connectDB = async () => {
+
+    try{
+         const conn = await mongoose.connect(ENV.DB_URL);
+         console.log("MongoDB connected Successfully ✅", conn.connection.host);
+    }
+
+    catch(error){
+       console.log("Error connecting to the Database ❌", error.message);
+       process.exit(1);
+    }
+}
